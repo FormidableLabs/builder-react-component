f0bf92ce2c87ca448aee0457e0669fd4af1a300f
Ryan Roemer <ryan@loose-bits.com>
2015-10-24 16:13:41 -0700

    Add eslint checking on archetype itself.

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -10,7 +10,7 @@ var path = require("path");
 var webpackCfg = require("../webpack/webpack.config.test");
 
 var MAIN_PATH = path.join(process.cwd(), "test/client/main.js");
-var PREPROCESSORS = {}
+var PREPROCESSORS = {};
 PREPROCESSORS[MAIN_PATH] = ["webpack"];
 
 module.exports = function (config) {
