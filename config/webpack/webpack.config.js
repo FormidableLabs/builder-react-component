8f3f3189703c32da3384f5a2230fd46bce8cb842
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-12-09 16:15:08 -0800

    BUG: Require resolve 'css' loader. Fixes #15

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
         loader: require.resolve("babel-loader")
       }, {
         test: /\.css$/,
-        loader: require.resolve("style-loader") + "!css"
+        loader: require.resolve("style-loader") + "!" + require.resolve("css")
       }, {
         test: /\.(png|jpg)$/,
         loader: require.resolve("url-loader") + "?limit=8192"
