036102f314ec9ae8d0897f44912571ed875f09c0
Ryan Roemer <ryan@loose-bits.com>
2016-01-08 18:55:48 -0800

    Provide archetype/init.js for prompts, derived data for templates.
    For: https://github.com/FormidableLabs/builder-init/issues/3
    
    Parallel 'builder-init' PR: https://github.com/FormidableLabs/builder-init/pull/10
    
    * Add 'init.js' with user 'prompts' and 'derived' fields
    * Update `package.json` to handle optional `packageGitHubOrg` field

--- init.js
+++ init.js
@@ -2,9 +2,61 @@
 
 /**
  * Archetype `init` configuration.
+ *
+ * See: https://github.com/FormidableLabs/builder-init/blob/master/README.md#archetype-data
+ * for structuring this configuration file.
  */
-// TODO: REMOVE AND IMPLEMENT PROMPTS
-// https://github.com/FormidableLabs/builder-init/issues/3
-/*eslint-disable no-console*/
-console.log("TODO HERE init.js");
-/*eslint-enable no-console*/
+module.exports = {
+  // Prompts are Inquirer question objects.
+  // https://github.com/SBoudrias/Inquirer.js#question
+  //
+  // `builder-init` accepts:
+  // - an array of question objects (with a `name` property)
+  // - an object of question objects (keyed by `name`)
+  //
+  prompts: {
+    licenseDate: {
+      message: "License date",
+      default: (new Date()).getFullYear().toString()
+    },
+    licenseOrg: {
+      message: "License organization (e.g., you or your company)",
+      validate: function (val) {
+        return !!val.trim() || "Must enter a license organization";
+      }
+    },
+    // See: https://github.com/npm/validate-npm-package-name
+    packageName: {
+      message: "Package / GitHub project name (e.g., 'whiz-bang-component')",
+      validate: function (val) {
+        return /^([a-z0-9]+\-?)+$/.test(val.trim()) || "Must be lower + dash-cased string";
+      }
+    },
+    packageGitHubOrg: {
+      message: "GitHub organization name (e.g., 'AcmeCorp')",
+      validate: function (val) {
+        return /^([^\s])*$/.test(val) || "Must be GitHub-valid organization username";
+      }
+    },
+    packageDescription: {
+      message: "Package description"
+    }
+  },
+
+  // Derived fields are asynchronous functions that are given the previous user
+  // input data of the form: `function (data, cb)`. They callback with:
+  // `(err, value)`.
+  derived: {
+    componentPath: function (data, cb) { cb(null, data.packageName); },
+    componentName: function (data, cb) {
+      // PascalCase (with first character capitalized).
+      var name = data.packageName
+        .replace(/^\s+|\s+$/g, "")
+        .replace(/(^|[-_ ])+(.)/g, function (match, first, second) {
+          return second.toUpperCase();
+        });
+
+      cb(null, name);
+    }
+  }
+};
