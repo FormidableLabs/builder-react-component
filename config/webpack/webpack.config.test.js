95c424a55c87a967e15c7a3f27f5105cbb2fdb95
Ryan Roemer <ryan@loose-bits.com>
2016-05-10 10:31:27 -0700

    Chore module pattern frontend (#40)
    * Add frontend module pattern.
    * Move template deps to dev archetype.
    * Move dev resolves only to webpack test configs.

--- config/webpack/webpack.config.test.js
+++ config/webpack/webpack.config.test.js
@@ -9,6 +9,16 @@ var prodCfg = require("./webpack.config");
 // Replace with `__dirname` if using in project root.
 var ROOT = process.cwd();
 
+// Stash the location of `<archetype-dev>/node_modules`
+//
+// A normal `require.resolve` looks at `package.json:main`. We instead want
+// just the _directory_ of the module. So use heuristic of finding dir of
+// package.json which **must** exist at a predictable location.
+var archetypeDevNodeModules = path.join(
+  path.dirname(require.resolve("builder-react-component-dev/package.json")),
+  "node_modules"
+);
+
 module.exports = {
   cache: true,
   context: path.join(ROOT, "test/client"),
@@ -21,8 +31,12 @@ module.exports = {
     alias: {
       // Allow root import of `src/FOO` from ROOT/src.
       src: path.join(ROOT, "src")
-    }
+    },
+    root: [archetypeDevNodeModules]
   }),
+  resolveLoader: {
+    root: [archetypeDevNodeModules]
+  },
   module: prodCfg.module,
   devtool: "source-map"
 };
