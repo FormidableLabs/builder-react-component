ce3ed319a2ad503ff056c39e224e5f2f1861dc2d
Ryan Roemer <ryan@loose-bits.com>
2017-09-21 15:49:25 -0700

    Go for named export in init

--- init/src/components/{{componentPath}}.js
+++ init/src/components/{{componentPath}}.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class <%= componentName %> extends React.Component {
+export class <%= componentName %> extends React.Component {
   render() {
     return <div>Edit me!</div>;
   }
--- init/test/client/spec/components/{{componentPath}}.spec.js
+++ init/test/client/spec/components/{{componentPath}}.spec.js
@@ -4,7 +4,7 @@
 import React from "react";
 // eslint import plugin does poorly with webpack aliases
 // eslint-disable-next-line import/no-unresolved
-import Component from "src/components/<%= componentPath %>";
+import { <%= componentName %> } from "src/components/<%= componentPath %>";
 import { mount, shallow } from "enzyme";
 
 describe("components/<%= componentPath %>", () => {
@@ -12,7 +12,7 @@ describe("components/<%= componentPath %>", () => {
   it("has expected content with deep render", () => {
     // This is a "deep" render that renders children + all into an actual
     // browser DOM node.
-    const wrapper = mount(<Component />);
+    const wrapper = mount(<<%= componentName %> />);
     const divNode = wrapper.find("div");
 
     expect(divNode.text()).to.equal("Edit me!");
@@ -21,7 +21,7 @@ describe("components/<%= componentPath %>", () => {
   it("has expected content with shallow render", () => {
     // This is a "shallow" render that renders only the current component
     // without using the actual DOM.
-    const wrapper = shallow(<Component />);
+    const wrapper = shallow(<<%= componentName %> />);
     const divNode = wrapper.find("div");
 
     expect(divNode).to.have.lengthOf(1);
