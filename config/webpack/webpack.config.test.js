beda8296f4aa355dedf796dddbff48f368cc2fa6
Ryan Roemer <ryan@loose-bits.com>
2017-09-21 12:31:00 -0700

    WIP: server-test manual web test works now.

--- config/webpack/webpack.config.test.js
+++ config/webpack/webpack.config.test.js
@@ -6,22 +6,11 @@ var path = require("path");
 var _ = require("builder-react-component-dev/require")("lodash");
 var prodCfg = require("./webpack.config");
 
-// Stash the location of `<archetype-dev>/node_modules`
-//
-// A normal `require.resolve` looks at `package.json:main`. We instead want
-// just the _directory_ of the module. So use heuristic of finding dir of
-// package.json which **must** exist at a predictable location.
-var archetypeDevNodeModules = path.join(
-  path.dirname(require.resolve("builder-react-component-dev/package.json")),
-  "node_modules"
-);
-
 module.exports = {
   cache: true,
   context: path.resolve("test/client"),
   entry: "./main",
   externals: {
-    "cheerio": "window",
     "react/addons": true,
     "react/lib/ExecutionEnvironment": true,
     "react/lib/ReactContext": true
@@ -34,12 +23,8 @@ module.exports = {
     alias: {
       // Allow root import of `src/FOO` from ROOT/src.
       src: path.resolve("src")
-    },
-    root: [archetypeDevNodeModules]
+    }
   }),
-  resolveLoader: {
-    root: [archetypeDevNodeModules]
-  },
   module: prodCfg.module,
   devtool: "source-map"
 };
