113b50544ce9ec426379d7ea34ec34b3c6e2d7c9
Ryan Roemer <ryan@loose-bits.com>
2017-09-21 15:09:36 -0700

    WIP: Get tests working

--- init/test/client/spec/components/{{componentPath}}.spec.js
+++ init/test/client/spec/components/{{componentPath}}.spec.js
@@ -2,37 +2,30 @@
  * Client tests
  */
 import React from "react";
+// eslint import plugin does poorly with webpack aliases
+// eslint-disable-next-line import/no-unresolved
 import Component from "src/components/<%= componentPath %>";
-// Use `TestUtils` to inject into DOM, simulate events, etc.
-// See: https://facebook.github.io/react/docs/test-utils.html
-import TestUtils from "react-addons-test-utils";
+import { mount, shallow } from "enzyme";
 
 describe("components/<%= componentPath %>", () => {
 
   it("has expected content with deep render", () => {
     // This is a "deep" render that renders children + all into an actual
     // browser DOM node.
-    //
-    // https://facebook.github.io/react/docs/test-utils.html#renderintodocument
-    const rendered = TestUtils.renderIntoDocument(<Component />);
+    const wrapper = mount(<Component />);
+    const divNode = wrapper.find("div");
 
-    // This is a real DOM node to assert on.
-    const divNode = TestUtils
-      .findRenderedDOMComponentWithTag(rendered, "div");
-
-    expect(divNode).to.have.property("innerHTML", "Edit me!");
+    expect(divNode.text()).to.equal("Edit me!");
   });
 
   it("has expected content with shallow render", () => {
     // This is a "shallow" render that renders only the current component
     // without using the actual DOM.
-    //
-    // https://facebook.github.io/react/docs/test-utils.html#shallow-rendering
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<Component />);
-    const output = renderer.getRenderOutput();
+    const wrapper = shallow(<Component />);
+    const divNode = wrapper.find("div");
 
-    expect(output.type).to.equal("div");
-    expect(output.props.children).to.contain("Edit me");
+    expect(divNode).to.have.lengthOf(1);
+    expect(divNode.text()).to.equal("Edit me!");
   });
+
 });
