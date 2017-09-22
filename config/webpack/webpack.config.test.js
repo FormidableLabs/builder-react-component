4684e6e0ebfbb578c377fbc3804b408b96382b68
Ryan Roemer <ryan@loose-bits.com>
2017-09-22 15:38:02 -0700

    Add resolve/resolveLoader of dev archetype back.

--- config/webpack/webpack.config.test.js
+++ config/webpack/webpack.config.test.js
@@ -6,6 +6,16 @@ var path = require("path");
 var _ = require("builder-react-component-dev/require")("lodash");
 var prodCfg = require("./webpack.config");
 
+// Stash the location of `<archetype-dev>/node_modules`
+//
+// A normal `require.resolve` looks at `package.json:main`. We instead want
+// just the _directory_ of the module. So use heuristic of finding dir of
+// package.json which **must** exist at a predictable location.
+var archetypeDevNodeModules = path.join(
+  path.dirname(require.resolve("builder-react-component-dev/package.json")),
+  "node_modules"
+);
+
 module.exports = {
   cache: true,
   context: path.resolve("test/client"),
@@ -23,8 +33,12 @@ module.exports = {
     alias: {
       // Allow root import of `src/FOO` from ROOT/src.
       src: path.resolve("src")
-    }
+    },
+    modules: [archetypeDevNodeModules]
   }),
+  resolveLoader: {
+    modules: [archetypeDevNodeModules]
+  },
   module: prodCfg.module,
   devtool: "source-map"
 };
